91d4196754fcb45a4d8af5af1ec1a469fc6348f8
Remy Kooistra <remykooistra77@gmail.com>
2023-02-04 14:06:36 +0100

    Multiple tresholds support

--- treshold_check.py
+++ treshold_check.py
@@ -9,18 +9,22 @@ dotenv.load_dotenv()
 # Threshold for available storage, in GB
 THRESHOLD = int(os.getenv("STORAGE_THRESHOLD"))
 
+# List of mounts to check
+MOUNTS = os.getenv("MOUNTS_TO_CHECK", "/").split(",")
+
 # Discord webhook URL
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
-# Get the root partition usage information
-root_partition = disk_usage("/")
+for mount in MOUNTS:
+    # Get the usage information for the current mount
+    usage = disk_usage(mount)
 
-# Calculate the available storage on the root partition
-avail = root_partition.free // (1024**3)
+    # Calculate the available storage on the mount
+    avail = usage.free // (1024**3)
 
-# Check if available storage is below the threshold
-if avail < THRESHOLD:
-    # Send a Discord notification
-    requests.post(WEBHOOK_URL, json={
-        "content": f"Available storage on root partition is below {THRESHOLD}GB. Current available storage: {avail}GB"
-    })
+    # Check if available storage is below the threshold
+    if avail < THRESHOLD:
+        # Send a Discord notification
+        requests.post(WEBHOOK_URL, json={
+            "content": f"Available storage on {mount} is below {THRESHOLD}GB. Current available storage: {avail}GB"
+        })
